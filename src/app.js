faac7f9ac89069ae6d58f9d7dc7e4f1c5269879d
caluapps <office@calu-apps.at>
2018-05-24 22:32:03 +0200

    dropdown for picking sortBy;

--- src/app.js
+++ src/app.js
@@ -20,12 +20,16 @@ const store = configureStore();
 // getVisibleExpenses -> print visible ones to screen
 
 store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 109500}));
+
+
+/*
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
   store.dispatch(setTextFilter('bill'));
-}, 3000);
+}, 3000); */
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
