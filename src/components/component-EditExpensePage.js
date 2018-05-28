4dce3ea3d1ac855c0501afd544c1e068ec217456
caluapps <office@calu-apps.at>
2018-05-29 00:45:33 +0200

    wiring up edit expense;

--- src/components/component-EditExpensePage.js
+++ src/components/component-EditExpensePage.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { editExpense, removeExpense } from '../actions/action-expenses';
+import ExpenseForm from './component-ExpenseForm';
 
 const EditExpensePage = (props) => {
-  console.log(props);
+  // console.log(props);
   return (
     <div>
-      Editing the expense with id of {props.match.params.id}
+      {/* Editing the expense with id of {props.match.params.id} */}
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          // Dispatch the action to edit the expense
+          props.dispatch(editExpense(props.expense.id, expense));
+          // Redirect to the dashboard
+          props.history.push('/');
+
+          console.log('updated', expense);
+        }}
+      />
+      <button onClick={() => {
+        // console.log('expense', props.expense);
+        console.log('id', props.expense.id);
+        props.dispatch(removeExpense({ id: props.expense.id }));
+        props.history.push('/');
+      }}>
+        Remove
+      </button>
     </div>
   )
 };
 
-export default EditExpensePage;
+// Remove expense via dispatch and then redirect to dashboard
+
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/component-ExpenseForm.js
+++ src/components/component-ExpenseForm.js
@@ -4,22 +4,26 @@ import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
-// const date = new Date();
+/* const date = new Date();
 const now = moment();
-console.log(now.format('MMM Do, YYYY'));
+console.log(now.format('MMM Do, YYYY')); */
 
 // setup note state
 // setup onChange and value for textarea
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
-    calendarFocused: false,
-    error: ''
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
+      error: ''
+    }
+  }
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
